4c39963c7a0e3273dc06cfb3b1f24be688d8e1b5
kalifabakhath <kalifabakhath762@gmail.com>
2021-01-31 19:34:52 +0530

    Updates

---
+++ precache-manifest.ce85158badcbf077e0774f8e7ef10eb6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26ab5af4324424ea4197cc1abeed8b5b",
+    "url": "/Myproject/index.html"
+  },
+  {
+    "revision": "cf0462eed84f865b2f9c",
+    "url": "/Myproject/static/css/2.fe51dd2d.chunk.css"
+  },
+  {
+    "revision": "d9e54aa840e09eb92822",
+    "url": "/Myproject/static/css/main.5f1f1689.chunk.css"
+  },
+  {
+    "revision": "cf0462eed84f865b2f9c",
+    "url": "/Myproject/static/js/2.ad723738.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/Myproject/static/js/2.ad723738.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9e54aa840e09eb92822",
+    "url": "/Myproject/static/js/main.3d10b783.chunk.js"
+  },
+  {
+    "revision": "ecae58f0c6fd5f5ffba5",
+    "url": "/Myproject/static/js/runtime-main.05a39dca.js"
+  },
+  {
+    "revision": "30d06f4502f2bf0b186045244809c798",
+    "url": "/Myproject/static/media/kct.30d06f45.jpg"
+  },
+  {
+    "revision": "6d49105764b4a84d0fcda13cdc6d7c10",
+    "url": "/Myproject/static/media/post.6d491057.jpg"
+  }
+]);
